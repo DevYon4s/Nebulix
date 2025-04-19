223cb149d15f94c6f606602b63643b7e1733b001
suxci <bdu1702813@bdu.edu.et>
2025-04-19 11:01:54 +0300

    add hero component with cloud animation

---
+++ components/hero.jsx
@@ -0,0 +1,160 @@
+"use client";
+
+import { useState, useEffect, useRef } from "react";
+import * as THREE from "./three.r134.min";
+import CLOUDS from "./vanta.clouds.min";
+import { Dialog, DialogPanel } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+
+const navigation = [
+  { name: "Product", href: "#" },
+  { name: "Features", href: "#" },
+  { name: "Marketplace", href: "#" },
+  { name: "Company", href: "#" },
+];
+
+export default function Hero() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const vantaRef = useRef(null);
+
+  useEffect(() => {
+    const vantaEffect = CLOUDS({
+      el: vantaRef.current,
+      THREE,
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 300.0,
+      minWidth: 200.0,
+      speed: 1.9,
+    });
+
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, []);
+
+  return (
+    <div className="relative">
+      <div ref={vantaRef} className="relative ">
+        {/* Vanta.js background section */}
+        <header className="absolute inset-x-0 top-0 z-50">
+          <nav
+            aria-label="Global"
+            className="flex items-center justify-between p-6 lg:px-8"
+          >
+            <div className="flex lg:flex-1">
+              <a href="#" className="-m-1.5 p-1.5">
+                <img alt="logo" src="logo.png" className="h-8 w-auto" />
+              </a>
+            </div>
+            <div className="flex lg:hidden">
+              <button
+                type="button"
+                onClick={() => setMobileMenuOpen(true)}
+                className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+              >
+                <Bars3Icon aria-hidden="true" className="h-6 w-6" />
+              </button>
+            </div>
+            <div className="hidden lg:flex lg:gap-x-12">
+              {navigation.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className="text-1xl font-semibold leading-6 text-gray-900"
+                >
+                  {item.name}
+                </a>
+              ))}
+            </div>
+            <div className="hidden lg:flex lg:flex-1 lg:justify-end"></div>
+          </nav>
+          <Dialog
+            open={mobileMenuOpen}
+            onClose={setMobileMenuOpen}
+            className="lg:hidden"
+          >
+            <div className="fixed inset-0 z-50" />
+            <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+              <div className="flex items-center justify-between">
+                <a href="#" className="-m-1.5 p-1.5">
+                  <img alt="logo" src="logo.png" className="h-8 w-auto" />
+                </a>
+                <button
+                  type="button"
+                  onClick={() => setMobileMenuOpen(false)}
+                  className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                >
+                  <span className="sr-only">Close menu</span>
+                  <XMarkIcon aria-hidden="true" className="h-6 w-6" />
+                </button>
+              </div>
+              <div className="mt-6 flow-root">
+                <div className="-my-6 divide-y divide-gray-500/10">
+                  <div className="space-y-2 py-6">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                  <div className="py-6"></div>
+                </div>
+              </div>
+            </DialogPanel>
+          </Dialog>
+        </header>
+
+        <div className="relative isolate pt-20">
+          <div className="py-24 sm:py-32 lg:pb-40">
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+              <div className="mx-auto max-w-2xl text-center">
+                <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl mt-20">
+                  Build in the Cloud • Scale Without Limits.
+                </h1>
+                <p className="mt-6 text-lg leading-8 text-gray-600">
+                  Deploy faster. Scale smarter. Build without limits on
+                  Nebulix’s secure and global cloud platform.
+                </p>
+                <div className="mt-10 flex items-center justify-center gap-x-6">
+                  <a
+                    href="#"
+                    className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Get started
+                  </a>
+                  <a
+                    href="#"
+                    className="text-sm font-semibold leading-6 text-gray-900"
+                  >
+                    Learn more <span aria-hidden="true">→</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Rest of the page with white background */}
+      <div className="bg-white">
+        <div className="   sm:mr-10 sm:ml-10 flow-root sm:mt-15">
+          <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+            <img
+              alt="App screenshot"
+              src="https://cdn.dribbble.com/userupload/9638368/file/original-80cbf873574636a009b558d559d095f7.png?format=webp&resize=400x300&vertical=center"
+              width={2432}
+              height={500}
+              className="rounded-md shadow-2xl ring-1 ring-gray-900/10"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
