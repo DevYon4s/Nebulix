60d731c6b128a88a1e44527f1b03c9028b8fba32
suxci <bdu1702813@bdu.edu.et>
2025-04-19 12:44:04 +0300

    add logo cloud section

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Hero from "@/components/hero";
 import Features from "@/components/features";
+import Logos from "@/components/logos";
 export default function Home() {
   return (
     <>
       <Hero />
+      <Logos />
       <Features />
     </>
   );
---
+++ components/logos.jsx
@@ -0,0 +1,48 @@
+export default function Logos() {
+  return (
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <h2 className="text-center text-lg font-semibold leading-8 text-indigo-600">
+          Trusted by the world’s most innovative teams
+        </h2>
+        <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+          <img
+            alt="vercel"
+            src="https://images.seeklogo.com/logo-png/48/1/vercel-logo-png_seeklogo-480587.png"
+            width={158}
+            height={48}
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+          />
+          <img
+            alt="Vo"
+            src="https://logowik.com/content/uploads/images/t_v0-ai4437.logowik.com.webp"
+            width={158}
+            height={48}
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+          />
+          <img
+            alt="React"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThFGBTwrc-m2PX_3YlVq-RcE25W8wv96dBTw&s"
+            width={158}
+            height={48}
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+          />
+          <img
+            alt="Nextjs"
+            src="https://images.seeklogo.com/logo-png/32/2/next-js-logo-png_seeklogo-321806.png"
+            width={158}
+            height={48}
+            className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
+          />
+          <img
+            alt="postgres"
+            src="https://cdn.iconscout.com/icon/free/png-256/free-postgresql-logo-icon-download-in-svg-png-gif-file-formats--programming-langugae-freebies-pack-logos-icons-1175119.png?f=webp"
+            width={158}
+            height={48}
+            className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
